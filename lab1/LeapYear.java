c8751bdd641fa940973bdd9d350dd5d182ad0cb3
Geng <820239854@qq.com>
2018-12-09 09:46:44 +0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,7 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+
     /** Check whether this year us leap year. */
     public static boolean isLeapYear(int year) {
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
@@ -10,7 +11,8 @@ public class LeapYear {
             return false;
         }
     }
-    
+
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
