3c5aaf926d990883560eb42b3fa45161d847bf2c
Geng <820239854@qq.com>
2018-12-09 09:15:15 +0800

    v1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,14 @@
  */
 public class LeapYear {
 
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
